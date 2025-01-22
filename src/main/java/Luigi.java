02579fd113aba9896cf9f4a4aea90ec668648862
laishuya <joshualaisooya12345@gmail.com>
2025-01-22 18:25:48 +0800

    Add greeting

---
+++ src/main/java/Luigi.java
@@ -0,0 +1,12 @@
+public class Luigi {
+    public static void main(String[] args) {
+        greetUser();
+    }
+
+    public static void greetUser(){
+        System.out.println("""
+                Hello, I'm Luigi! How can I help you?
+                Bye!
+                """);
+    }
+}
