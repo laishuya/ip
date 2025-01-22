b7fb874942874eac237b989e91972603f07a2ad0
laishuya <joshualaisooya12345@gmail.com>
2025-01-22 21:18:12 +0800

    Create list to track items

--- src/main/java/Luigi.java
+++ src/main/java/Luigi.java
@@ -1,15 +1,33 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Luigi {
+    private static ArrayList<String> list = new ArrayList<>();
+
+    private static void addItem(String item) {
+        list.add(item);
+        System.out.println("added: " + item);
+    }
+
+    private static void printList() {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println((i + 1) + ". " + list.get(i));
+        }
+    }
+
     public static void main(String[] args) {
         greetUser();
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            String input = scanner.nextLine();
+            String input = scanner.nextLine().trim();
             if (input.equals("bye")) {
                 break;
             }
-            System.out.println(input);
+            if (input.equals("list")) {
+                printList();
+            } else {
+                addItem(input);
+            }
         }
         System.out.println("Bye!");
         scanner.close();
