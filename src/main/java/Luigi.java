1ebe5452145af3ccecf03f7bd6df315c60920b62
laishuya <joshualaisooya12345@gmail.com>
2025-01-22 20:38:51 +0800

    Add support for Echo

--- src/main/java/Luigi.java
+++ src/main/java/Luigi.java
@@ -3,6 +3,16 @@ import java.util.Scanner;
 public class Luigi {
     public static void main(String[] args) {
         greetUser();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(input);
+        }
+        System.out.println("Bye!");
+        scanner.close();
     }
 
     private static void greetUser() {
