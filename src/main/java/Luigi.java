536e841a5b8cb7b511bf5e7c1c09ffdfb82e04a7
laishuya <joshualaisooya12345@gmail.com>
2025-01-23 13:25:07 +0800

    Add error handling and switch statements

--- src/main/java/Luigi.java
+++ src/main/java/Luigi.java
@@ -4,14 +4,6 @@ import java.util.ArrayList;
 public class Luigi {
     private static ArrayList<Task> list = new ArrayList<>();
 
-    private static void addItem(String item) {
-        Task task = new Task(item);
-        list.add(task);
-        System.out.println("Got it! I've added this task:");
-        System.out.println("  " + task);
-        System.out.println("You now have " + list.size() + " tasks in your list");
-    }
-
     private static void printList() {
         System.out.println("Here are your tasks:");
         for (int i = 0; i < list.size(); i++) {
@@ -59,36 +51,71 @@ public class Luigi {
     public static void main(String[] args) {
         greetUser();
         Scanner scanner = new Scanner(System.in);
+
         while (true) {
             String input = scanner.nextLine().trim();
             if (input.equals("bye")) {
                 break;
             }
-            else if (input.equals("list")) {
-                printList();
-            }
-            else if (input.startsWith("mark")) {
-                int index = Integer.parseInt(input.split(" ")[1]) - 1;
-                mark(index);
-            }
-            else if (input.startsWith("unmark")) {
-                int index = Integer.parseInt(input.split(" ")[1]) - 1;
-                unmark(index);
-            }
-            else if (input.startsWith("todo")) {
-                String description = input.substring(5).trim();
-                addToDo(description);
-            }
-            else if (input.startsWith("deadline")) {
-                String[] parts = input.substring(9).trim().split(" /by ");
-                addDeadline(parts[0].trim(), parts[1].trim());
-            }
-            else if (input.startsWith("event")) {
-                String[] parts = input.substring(6).trim().split(" /from | /to ");
-                addEvent(parts[0].trim(), parts[1].trim(), parts[2].trim());
-            }
-            else {
-                addItem(input);
+
+            String command = input.split(" ")[0];
+            switch (command) {
+                case "list":
+                    printList();
+                    break;
+
+                case "mark":
+                    try {
+                        int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                        mark(index);
+                    } catch (Exception e) {
+                        System.out.println("Invalid index!");
+                    }
+                    break;
+
+                case "unmark":
+                    try {
+                        int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                        unmark(index);
+                    } catch (Exception e) {
+                        System.out.println("Invalid index!");
+                    }
+                    break;
+
+                case "todo":
+                    try {
+                        String description = input.substring(5).trim();
+                        addToDo(description);
+                    } catch (Exception e) {
+                        System.out.println("Description of todo cannot be empty!");
+                        System.out.println("Correct example: todo read book");
+                    }
+                    break;
+
+                case "deadline":
+                    try {
+                        String[] deadlineParts = input.substring(9).trim().split(" /by ");
+                        addDeadline(deadlineParts[0].trim(), deadlineParts[1].trim());
+                    } catch (Exception e) {
+                        System.out.println("Please input description and deadline!");
+                        System.out.println("Correct example: deadline return book /by tomorrow");
+                    }
+                    break;
+
+                case "event":
+                    try {
+                        String[] eventParts = input.substring(6).trim().split(" /from | /to ");
+                        addEvent(eventParts[0].trim(), eventParts[1].trim(), eventParts[2].trim());
+                    } catch (Exception e) {
+                        System.out.println("Please input description, from (what time), to (what time)!");
+                        System.out.println("Correct example: event book club meeting " +
+                            "/from Tuesday 3 p.m. /to Tuesday 5 p.m.");
+                     }
+                    break;
+
+                default:
+                    System.out.println("Sorry, invalid command");
+                    break;
             }
         }
         System.out.println("Bye!");
