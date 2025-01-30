e56d3fa55abdb97bda6aa426baeb15dc9fed7110
laishuya <joshualaisooya12345@gmail.com>
2025-01-30 12:12:34 +0800

    Implement find command for level 9

---
+++ src/main/java/luigi/commands/FindCommand.java
@@ -0,0 +1,27 @@
+package luigi.commands;
+
+import luigi.tasks.Task;
+import luigi.TaskList;
+import luigi.Ui;
+import luigi.Storage;
+
+import java.util.ArrayList;
+
+public class FindCommand extends Command {
+    private final String word;
+
+    public FindCommand(String word){
+        this.word = word;
+    }
+
+    @Override
+    public void execute(TaskList list, Ui ui, Storage storage) {
+        ArrayList<Task> matchingTasks = list.findTasksWithSameWord(word);
+        ui.sayMessage("Here are the matching tasks in your list:");
+        int index = 1;
+        for (Task task : matchingTasks) {
+            ui.sayMessage(index + ". " + task);
+            index++;
+        }
+    }
+}
